2a3ed6eaeaf7faef23a2182d92dba5eaf58f068a
Lachlan Gordon <lachlanwgordon@gmail.com>
2021-05-23 11:26:00 +1000

    Added Ooui, made basic page in code

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Ooui.Forms;
+using Ooui;
+using Label = Xamarin.Forms.Label;
+
+namespace PiLiveRecorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Forms.Init();
+            UI.Host = "10.1.1.155";
+            var page = new ContentPage();
+            var content = new StackLayout
+            {
+                Children = 
+                {
+                    new Label{Text = "hellooww"}
+                }
+            };
+            page.Content = content;
+
+            var element = page.GetOouiElement();
+
+            UI.Publish("/", element);
+
+            Console.ReadLine();
+        }
+    }
+}
